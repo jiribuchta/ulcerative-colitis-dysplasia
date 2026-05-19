801c4d383e553d98fbf738a7d778dab3fea8bca2
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-05-19 20:09:50 +0000

    feat: embedding dataset

--- ml/data/datasets/embeddings.py
+++ ml/data/datasets/embeddings.py
@@ -1,10 +1,9 @@
 from collections.abc import Iterable
 from typing import Any, cast
 
-from albumentations.core.composition import TransformType
-from albumentations.pytorch import ToTensorV2
+import torch
 from datasets import Dataset as HFDataset
-from rationai.mlkit.data.datasets import MetaTiledSlides, OpenSlideTilesDataset
+from rationai.mlkit.data.datasets import MetaTiledSlides
 from torch.utils.data import Dataset
 
 from ml.data.datasets.labels import LabelMode, get_label, process_slides
@@ -12,111 +11,93 @@ from ml.data.datasets.utils import filter_tiles
 from ml.typing import Metadata, PredictSample, Sample
 
 
-class SlideTiles[T: Sample | PredictSample](Dataset[T]):
+class _Embeddings[T: Sample | PredictSample](Dataset[T]):
     def __init__(
         self,
         slide_metadata: dict[str, Any],
         tiles: HFDataset,
         mode: LabelMode | str | None,
         include_labels: bool = True,
-        transforms: TransformType | None = None,
     ) -> None:
-        super().__init__()
-        self.slide_tiles = OpenSlideTilesDataset(
-            slide_path=slide_metadata["path"],
-            level=slide_metadata["level"],
-            tile_extent_x=slide_metadata["tile_extent_x"],
-            tile_extent_y=slide_metadata["tile_extent_y"],
-            tiles=tiles,
-        )
         self.slide_metadata = slide_metadata
+        self.tiles = tiles
         self.mode = LabelMode(mode) if mode is not None else None
         self.include_labels = include_labels
-        self.transforms = transforms
-        self.to_tensor = ToTensorV2()
 
         if self.include_labels and self.mode is None:
             raise ValueError("Mode must be specified if labels are included.")
 
     def __len__(self) -> int:
-        return len(self.slide_tiles)
+        return len(self.tiles)
 
     def __getitem__(self, idx: int) -> T:
-        image = self.slide_tiles[idx]
+        tile = self.tiles[idx]
         metadata = Metadata(
-            slide_name=self.slide_tiles.slide_path.stem,
-            x=self.slide_tiles.tiles[idx]["x"],
-            y=self.slide_tiles.tiles[idx]["y"],
+            slide_name=self.slide_metadata["name"],
+            x=tile["x"],
+            y=tile["y"],
         )
 
-        if self.transforms is not None:
-            image = self.transforms(image=image)["image"]
-
-        image = self.to_tensor(image=image)["image"]
-
+        embedding = torch.tensor(tile["embedding"])
         if not self.include_labels:
-            return cast("T", (image, metadata))
+            return cast("T", (embedding, metadata))
 
         assert self.mode is not None, "Mode must be specified for labels."
-        label = get_label(self.slide_metadata, self.mode)
-        return cast("T", (image, label, metadata))
+        label = get_label(tile, self.mode)
+        return cast("T", (embedding, label, metadata))
 
 
-class Tiles(MetaTiledSlides[Sample]):
+class Embeddings(MetaTiledSlides[Sample]):
     def __init__(
         self,
         uris: Iterable[str] | str,
         mode: LabelMode | str,
-        transforms: TransformType | None = None,
         thresholds: dict[str, float] | None = None,
         val_fold: int | None = None,
         is_val: bool = False,
     ) -> None:
-        self.transforms = transforms
         self.mode = LabelMode(mode)
         self.thresholds = thresholds or {}
         self.val_fold = val_fold
         self.is_val = is_val
         super().__init__(uris=(uris,) if isinstance(uris, str) else uris)
 
-    def generate_datasets(self) -> Iterable[SlideTiles[Sample]]:
+    def generate_datasets(self) -> Iterable[_Embeddings[Sample]]:
         self.slides = process_slides(self.slides, self.val_fold, self.is_val)
         return (
-            SlideTiles(
+            _Embeddings(
                 slide_metadata=dict(slide),
                 tiles=filter_tiles(
                     self.filter_tiles_by_slide(dict(slide)["id"]), self.thresholds
                 ),
                 mode=self.mode,
                 include_labels=True,
-                transforms=self.transforms,
             )
             for slide in self.slides
         )
 
 
-class TilesPredict(MetaTiledSlides[PredictSample]):
+class EmbeddingsPredict(MetaTiledSlides[PredictSample]):
     def __init__(
         self,
         uris: Iterable[str] | str,
-        transforms: TransformType | None = None,
+        mode: LabelMode | str,
         thresholds: dict[str, float] | None = None,
     ) -> None:
-        self.transforms = transforms
+        self.mode = LabelMode(mode)
         self.thresholds = thresholds or {}
         super().__init__(uris=(uris,) if isinstance(uris, str) else uris)
 
-    def generate_datasets(self) -> Iterable[SlideTiles[PredictSample]]:
+    def generate_datasets(self) -> Iterable[_Embeddings[PredictSample]]:
         self.slides = process_slides(self.slides)
         return (
-            SlideTiles(
+            _Embeddings(
                 slide_metadata=dict(slide),
                 tiles=filter_tiles(
                     self.filter_tiles_by_slide(dict(slide)["id"]), self.thresholds
                 ),
-                mode=None,
+                mode=self.mode,
                 include_labels=False,
-                transforms=self.transforms,
             )
             for slide in self.slides
         )
--- ml/typing.py
+++ ml/typing.py
@@ -5,28 +5,18 @@ from torch import Tensor
 
 class Metadata(TypedDict):
     slide_name: str
+    x: int
+    y: int
 
 
 class MetadataBatch(TypedDict):
     slide_name: list[str]
+    x: Tensor
+    y: Tensor
 
 
 type Sample = tuple[Tensor, Tensor, Metadata]
 type PredictSample = tuple[Tensor, Metadata]
 
-
-class MetadataTiles(Metadata):
-    x: int
-    y: int
-
-
-class MetadataTilesBatch(MetadataBatch):
-    x: Tensor
-    y: Tensor
-
-
-type TilesSample = tuple[Tensor, Tensor, MetadataTiles]
-type TilesPredictSample = tuple[Tensor, MetadataTiles]
-
-type TilesInput = tuple[Tensor, Tensor, MetadataTilesBatch]
-type TilesPredictInput = tuple[Tensor, MetadataTilesBatch]
+type Input = tuple[Tensor, Tensor, MetadataBatch]
+type PredictInput = tuple[Tensor, MetadataBatch]

11911053d824e91789db4b38e73dbefc92a60a46
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-07-08 19:16:50 +0000

    feat: update tile masks configuration and processing logic

--- preprocessing/tile_masks.py
+++ preprocessing/tile_masks.py
@@ -4,13 +4,14 @@ from pathlib import Path
 from typing import Any
 
 import hydra
-import mlflow
+import mlflow.artifacts
 import numpy as np
 import pandas as pd
 import pyvips
 import ray
 import torch
 from omegaconf import DictConfig
+from ratiopath.openslide import OpenSlide
 from rationai.masks import process_items, tile_mask, write_big_tiff
 from rationai.masks.mask_builders import ScalarMaskBuilder
 from rationai.mlkit import autolog, with_cli_args
@@ -19,12 +20,17 @@ from rationai.mlkit.lightning.loggers import MLFlowLogger
 
 @ray.remote
 def process_slide(
-    slide: Any,
+    slide: pd.Series,
     percentage_cols: list[str],
     output_path: Path,
-    tiles: Any,
+    tiles: pd.DataFrame,
+    level: int,
 ) -> None:
     slide_tiles = tiles[tiles["slide_id"] == slide.id]
+
+    with OpenSlide(slide["path"]) as slide_wsi:
+        mask_extent_x, mask_extent_y = slide_wsi.level_dimensions[level]
+        mpp_x, mpp_y = slide_wsi.slide_resolution(level)
 
     for percentage_col in [*percentage_cols]:
         filename = f"{Path(slide.path).stem}.tiff"
@@ -33,10 +39,10 @@ def process_slide(
         builder = ScalarMaskBuilder(
             save_dir,
             filename,
-            slide.extent_x,
-            slide.extent_y,
-            slide.mpp_x,
-            slide.mpp_y,
+            mask_extent_x,
+            mask_extent_y,
+            mpp_x,
+            mpp_y,
             slide.tile_extent_x,
             slide.stride_x,
         )
@@ -52,15 +58,15 @@ def process_slide(
     mask = tile_mask(
         slide_tiles,
         tile_extent=(slide.tile_extent_x, slide.tile_extent_y),
-        size=(slide.extent_x, slide.extent_y),
+        size=(mask_extent_x, mask_extent_y),
     )
 
     mask_path = output_path / "outlines" / f"{Path(slide.path).stem}.tiff"
     write_big_tiff(
         pyvips.Image.new_from_array(np.array(mask)),
         mask_path,
-        mpp_x=slide.mpp_x,
-        mpp_y=slide.mpp_y,
+        mpp_x=mpp_x,
+        mpp_y=mpp_y,
     )
 
 
@@ -68,37 +74,34 @@ def process_slide(
 @hydra.main(config_path="../configs", config_name="preprocessing", version_base=None)
 @autolog
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
+    output_path = Path(config.output_dir)
+    output_path.mkdir(exist_ok=True, parents=True)
 
-    for name, uri in config.mlflow_uris.filtered.items():
-        if name != "test_preliminary":
-            continue
+    for percentage_col in [*config.percentage_cols, "outlines"]:
+        (output_path / str(percentage_col)).mkdir(parents=True, exist_ok=True)
 
-        local_path = mlflow.artifacts.download_artifacts(uri)
+    for name, uri in config.dataset.mlflow_uris.filtered.items():
+        local_path = Path(mlflow.artifacts.download_artifacts(uri))
 
-        slides = pd.read_parquet(Path(local_path) / "slides.parquet")
-        tiles = pd.read_parquet(Path(local_path) / "tiles.parquet")
+        slides = pd.read_parquet(local_path / "slides")
+        tiles = pd.read_parquet(local_path / "tiles")
 
-        output_path = Path(config.output_dir) / name
-        output_path.mkdir(exist_ok=True, parents=True)
-
-        for percentage_col in [
-            *config.percentage_cols,
-            "outlines",
-        ]:
-            (output_path / percentage_col).mkdir(parents=True, exist_ok=True)
 
         process_items(
-            slides.itertuples(),
+            (slide for _, slide in slides.iterrows()),
             process_slide,
             fn_kwargs={
                 "percentage_cols": config.percentage_cols,
                 "output_path": output_path,
                 "tiles": tiles,
+                "level": config.level,
             },
             max_concurrent=config.max_concurrent,
         )
 
-        logger.log_artifacts(output_path, artifact_path=f"tile_masks/{name}")
+        logger.log_artifacts(
+            str(output_path), artifact_path=config.mlflow_artifact_path
+        )
 
 
 if __name__ == "__main__":

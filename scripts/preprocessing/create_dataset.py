9ca46800472ba913b90bad621b36acb4b671c20d
Jiří Buchta <jiribuchta02@gmail.com>
2026-07-31 12:49:40 +0200

    fix: remove kube-jobs from dep groups (cluster provides it)

--- scripts/preprocessing/create_dataset.py
+++ scripts/preprocessing/create_dataset.py
@@ -9,9 +9,9 @@ submit_job(
     memory="16Gi",
     shm="16Gi",
     script=[
-        "git clone https://github.com/jiribuchta/ulcerative-colitis-dysplasia.git@feature/prov workdir",
+        "git clone --branch feature/prov https://github.com/jiribuchta/ulcerative-colitis-dysplasia.git workdir",
         "cd workdir",
-        "uv sync --frozen",
+        "uv sync",
         "export MLFLOW_TRACKING_URI=http://mlflow-jiribuchta.rationai-mlflow:5000/ PYTHONUNBUFFERED=1 MLFLOW_USER=jiribuchta && uv run python -u -m preprocessing.create_dataset +dataset=raw",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],

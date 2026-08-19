72410837a519d1fc6b0191db2a527eb4584a4b6e
Jiří Buchta <jiribuchta02@gmail.com>
2026-08-19 14:27:00 +0200

    fix: update path handling in select function and adjust artifact download paths in main function

--- preprocessing/tiling.py
+++ preprocessing/tiling.py
@@ -178,6 +178,7 @@ def filter_tissue(row: dict[str, Any], threshold: float) -> bool:
 def select(row: dict[str, Any], target_labels: list[str]) -> dict[str, Any]:
     selected_row = {
         "slide_id": row["slide_id"],
+        "path": row["path"],
         "x": row["tile_x"],
         "y": row["tile_y"],
         "tissue": row["tissue"],
@@ -281,11 +282,13 @@ def tiling(
 @hydra.main(config_path="../configs", config_name="preprocessing", version_base=None)
 @autolog
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
-    qc_folder = Path(download_artifacts(config.mlflow_uris.qc))
-    tissue_folder = Path(download_artifacts(config.mlflow_uris.tissue))
-    annot_folder = Path(config.annot_path)
+    print(f"Running tiling preprocessing with config: {config.dataset.mlflow_uris.qc}")
+    print(f"Running tiling preprocessing with config: {config}")
+    qc_folder = Path(download_artifacts(config.dataset.mlflow_uris.qc))
+    tissue_folder = Path(download_artifacts(config.dataset.mlflow_uris.tissue))
+    annot_folder = Path(config.dataset.annot_path)
 
-    for name, split_uri in config.mlflow_uris.splits.items():
+    for name, split_uri in config.dataset.mlflow_uris.splits.items():
         split = pd.read_csv(
             mlflow.artifacts.download_artifacts(split_uri), index_col="slide_id"
         )

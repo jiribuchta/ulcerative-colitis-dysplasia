422053ccabe4e13636e2c38f8c891b6c81fd9eb2
Jiří Buchta <jiribuchta02@gmail.com>
2026-08-22 14:53:05 +0200

    chore: add level2_extent512 embeddings and update training configuration

--- scripts/ml/train.py
+++ scripts/ml/train.py
@@ -1,19 +1,20 @@
 from kube_jobs import storage, submit_job
 
-
 submit_job(
     job_name="ulcerative-colitis-dysplasia-train-new",
-    username="...",
+    username="jiribuchta",
+    image="cerit.io/jiri_buchta/base-test-cuda:0.0.7",
     public=False,
     cpu=16,
     memory="64Gi",
     shm="64Gi",
-    gpu="mig-1g.10gb",
+    gpu="A40",
     script=[
-        "git clone -b feature/ml-cnn https://github.com/RationAI/ulcerative-colitis-dysplasia.git workdir",
+        "git clone https://github.com/jiribuchta/ulcerative-colitis-dysplasia.git workdir",
         "cd workdir",
-        "uv sync --frozen",
-        "uv run python -m ml '+experiment=ml/train/virchow2_l...' val_fold=0,1,2,3,4 --multirun",
+        "git checkout feature/ml-cnn",
+        "uv sync",
+        "export MLFLOW_TRACKING_URI=http://mlflow-jiribuchta.rationai-mlflow:5000/ PYTHONUNBUFFERED=1 MLFLOW_USER=jiribuchta && uv run python -u -m ml '+experiment=ml/train/virchow2_l1_epi0_allslides' val_fold=0,1,2,3,4 --multirun",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )

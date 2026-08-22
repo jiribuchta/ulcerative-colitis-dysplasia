1c69973fb6c0af2c22d106919870c368a7322433
Jiří Buchta <jiribuchta02@gmail.com>
2026-08-22 15:01:56 +0200

    Refactor code structure for improved readability and maintainability

--- scripts/preprocessing/embeddings.py
+++ scripts/preprocessing/embeddings.py
@@ -2,17 +2,19 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="ulcerative-colitis-dysplasia-embeddings-...",
-    username=...,
+    job_name="ulcerative-colitis-dysplasia-embeddings",
+    username="jiribuchta",
+    image="cerit.io/jiri_buchta/base-test-cuda:0.0.7",
     public=False,
     cpu=8,
     memory="32Gi",
     shm="16Gi",
     script=[
-        "git clone https://github.com/RationAI/ulcerative-colitis.git workdir",
+        "git clone https://github.com/jiribuchta/ulcerative-colitis-dysplasia.git workdir",
         "cd workdir",
-        "uv sync --frozen",
-        "uv run --active -m preprocessing.embeddings +dataset=tiled_filtered/...",
+        "git checkout feature/embeddings",
+        "uv sync",
+        "export MLFLOW_TRACKING_URI=http://mlflow-jiribuchta.rationai-mlflow:5000/ PYTHONUNBUFFERED=1 MLFLOW_USER=jiribuchta && uv run python -u -m preprocessing.embeddings +dataset=tiled_filtered/level2_extent512",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )

3592f5773536f1e4e615d95b2a64f32927216661
Jiří Buchta <jiribuchta02@gmail.com>
2026-08-24 12:10:29 +0200

    fix: update job submission parameters and repository URL in tissue_masks.py fix: adjust max_concurrent parameter in qc.yaml feat: log provenance in tissue_masks processing

--- preprocessing/tissue_masks.py
+++ preprocessing/tissue_masks.py
@@ -17,7 +17,7 @@ from rationai.masks import (
 )
 from rationai.mlkit import autolog, with_cli_args
 from rationai.mlkit.lightning.loggers import MLFlowLogger
-
+from rationai.mlkit.provenance import log_provenance
 
 @ray.remote(memory=4 * 1024**3)
 def process_slide(slide_path: str, level: int, output_path: Path) -> None:
@@ -59,6 +59,7 @@ def main(config: DictConfig, logger: MLFlowLogger) -> None:
         )
 
         logger.log_artifacts(str(tmpdir_path), config.mlflow_artifact_path)
+        log_provenance(logger=logger, config=config)
 
 
 if __name__ == "__main__":

d3f8712ee7cc9a7625a827859d96c03e8bab3ded
Jiří Buchta <jiribuchta02@gmail.com>
2026-08-26 23:32:55 +0200

    chore: update mlflow URIs and training script for vgg16 model

--- scripts/ml/train.py
+++ scripts/ml/train.py
@@ -14,7 +14,7 @@ submit_job(
         "git clone -b feature/ml-cnn https://github.com/jiribuchta/ulcerative-colitis-dysplasia.git workdir",
         "cd workdir",
         "uv sync",
-        "export MLFLOW_TRACKING_URI=http://mlflow-jiribuchta.rationai-mlflow:5000/ PYTHONUNBUFFERED=1 MLFLOW_USER=jiribuchta && uv run python -m ml '+experiment=ml/train/virchow2_l2_epi0_allslides' val_fold=0,1,2,3,4 --multirun",
+        "export MLFLOW_TRACKING_URI=http://mlflow-jiribuchta.rationai-mlflow:5000/ PYTHONUNBUFFERED=1 MLFLOW_USER=jiribuchta && uv run python -m ml '+experiment=ml/train/vgg16' +dataset=tiled_filtered/level2_extent224 val_fold=0,1,2,3,4 --multirun",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )
